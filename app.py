9587e59a20d3987f69fd4c1c920cad3a7b8529d5
smurfie07 <saif.msrj@gmail.com>
2023-12-03 04:54:15 +0500

    tweak

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
 import random
-
+import os
 app = Flask(__name__)
-rgb = (0, 0, 0)
+rgb = (
+    os.environ.get("DEFAULT_COLOR")
+    if os.environ.get("DEFAULT_COLOR")
+    else (0, 128, 128)
+    )
 @app.route("/")
 def randColorGen():
-    rgb = (random.randint(0,255), 
-           random.randint(0,255),
-           random.randint(0,255),)
+    rgb = (
+        random.randint(0,255), 
+        random.randint(0,255),
+        random.randint(0,255),
+        )
     rgbFormula = "rgb" + str(rgb)
     return render_template(
         "main.html",
